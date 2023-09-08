13687e55922014001675c02704036ae3783d8cbb
gamiken <gamiken@kappa-lab.com>
2023-09-08 11:34:23 +0900

    add e2e

--- Makefile
+++ Makefile
@@ -4,6 +4,11 @@ run:
 check:
 	curl http://localhost:8088
 
-
 build:
 	go build -o=app ./cmd
+	chmod +x app
+
+run-binary:
+	./app
+
+PHONY: build run	
--- cmd/main.go
+++ cmd/main.go
@@ -9,6 +9,11 @@ func main() {
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(rw, getWorld())
 	})
+
+	http.HandleFunc("/404", func(rw http.ResponseWriter, r *http.Request) {
+		rw.WriteHeader(404)
+	})
+
 	http.ListenAndServe(":8088", nil)
 }
 
